a5fcc8cf3fee4d86fc511a3a2e2afebd57cb3ccf
anonymous20041 <145598609+anonymous20041@users.noreply.github.com>
2026-05-01 18:19:41 +0530

    Create App component for landing page

---
+++ App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="landing-page">
+      <header className="landing-content">
+        <h1>Paradise Nursery</h1>
+        <p>Bringing nature into your home with our beautiful selection of houseplants.</p>
+        <button>Get Started</button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
